3fc46b46b93a25dadeb10e11de1a8135e2c3fcdb
Vaibhav <sareenvaib12@gmail.com>
2023-12-04 14:25:50 +0530

    first commit

---
+++ next.config.js
@@ -0,0 +1,10 @@
+/** @type {import('next').NextConfig} */
+const nextConfig = {
+    images: {
+        domains: [
+            "fucluspvtrydyreypsnv.supabase.co"
+        ]
+    }
+}
+
+module.exports = nextConfig
